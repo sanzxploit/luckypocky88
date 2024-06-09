47ebbac250512cddf4c39667d0046782ac3ef25b
sanzxploit <146035697+sanzxploit@users.noreply.github.com>
2024-06-09 23:19:03 +0700

    Create index.php

---
+++ xploit/index.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Narrow Jumbotron Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../css/editor.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="narrow-jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+        <!-- START: NAVBAR BOTTOM MOBILE -->
+        <div id="navbar" class="navbar navbar-expand d-md-none d-lg-none d-xl-none fixed-bottom" style="background-color: #000000;padding:10px 10px 10px 10px;">
+            <a href="https://watitoto124.com/index.html#/mine" class="btn btn-theme" style="width:30%;">
+                <b>LOGIN
+</b></a>
+<a href="http://LUCKYPOCKY88indo.com/WT2" class="btn btn-theme" style="width:70%;margin-left:10px;">
+    <b>DAFTAR SEKARANG
+</b></a></div>
+<!-- END -->
+         <noscript>
+            <div class='noscript'>
+            <p><span>&#9888;</span></p>
+            <p>Please Enable Javascript to view our site content</p>
+            <p>Thank you</p>
+            </div>
+            </noscript>
+        <script type="text/javascript">
+            //<![CDATA[
+            // Inspect
+            var message="SITUS INI DI LINDUNGI PROTEKSI LUCKYPOCKY88 COBA HUBUNGIN DEEPWEB !!!";function clickIE4(){if(2==event.button)return alert(message),!1}function clickNS4(e){if((document.layers||document.getElementById&&!document.all)&&(2==e.which||3==e.which))return alert(message),!1}document.layers?(document.captureEvents(Event.MOUSEDOWN),document.onmousedown=clickNS4):document.all&&!document.getElementById&&(document.onmousedown=clickIE4),document.oncontextmenu=new Function("alert(message);return false");
+            !function t(){try{!function t(n){1===(""+n/n).length&&0!==n||function(){}.constructor("debugger")(),t(++n)}(0)}catch(n){setTimeout(t,5e3)}}();
+            shortcut={all_shortcuts:{},add:function(e,t,a){var o={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(a)for(var r in o)void 0===a[r]&&(a[r]=o[r]);else a=o;o=a.target,"string"==typeof a.target&&(o=document.getElementById(a.target)),e=e.toLowerCase(),r=function(o){var r;if((o=o||window.event,a.disable_in_input)&&(o.target?r=o.target:o.srcElement&&(r=o.srcElement),3==r.nodeType&&(r=r.parentNode),"INPUT"==r.tagName||"TEXTAREA"==r.tagName))return;o.keyCode?code=o.keyCode:o.which&&(code=o.which),r=String.fromCharCode(code).toLowerCase(),188==code&&(r=","),190==code&&(r=".");var n=e.split("+"),c=0,l={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},s={esc:27,escape:27,tab:9,space:32,return:13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,break:19,insert:45,home:36,delete:46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},i=!1,d=!1,p=!1,u=!1,h=!1,f=!1,g=!1,v=!1;o.ctrlKey&&(u=!0),o.shiftKey&&(d=!0),o.altKey&&(f=!0),o.metaKey&&(v=!0);for(var y=0;k=n[y],y<n.length;y++)"ctrl"==k||"control"==k?(c++,p=!0):"shift"==k?(c++,i=!0):"alt"==k?(c++,h=!0):"meta"==k?(c++,g=!0):1<k.length?s[k]==code&&c++:a.keycode?a.keycode==code&&c++:r==k?c++:l[r]&&o.shiftKey&&(r=l[r],r==k&&c++);if(c==n.length&&u==p&&d==i&&f==h&&v==g&&(t(o),!a.propagate))return o.cancelBubble=!0,o.returnValue=!1,o.stopPropagation&&(o.stopPropagation(),o.preventDefault()),!1},this.all_shortcuts[e]={callback:r,target:o,event:a.type},o.addEventListener?o.addEventListener(a.type,r,!1):o.attachEvent?o.attachEvent("on"+a.type,r):o["on"+a.type]=r},remove:function(e){e=e.toLowerCase();var t=this.all_shortcuts[e];if(delete this.all_shortcuts[e],t){e=t.event;var a=t.target;t=t.callback;a.detachEvent?a.detachEvent("on"+e,t):a.removeEventListener?a.removeEventListener(e,t,!1):a["on"+e]=!1}}},shortcut.add("Ctrl+U",function(){alert("SITUS INI DI LINDUNGI PROTEKSI LUCKYPOCKY88 COBA TEKAN F12")}),shortcut.add("f12",function(){alert("SITUS INI DI LINDUNGI PROTEKSI LUCKYPOCKY88 COBA TEKAN CTRL + U")}),shortcut.add("Meta+Alt+U",function(){alert("KAMU KURANG HEBAT BERARTI !!!")}),shortcut.add("Ctrl+C",function(){alert("KAMU KURANG HEBAT BOSS !!!")}),shortcut.add("Meta+C",function(){alert("KAMU KRANG HEBAT BOSKU !!!")});
+            //]]>
+            </script>
+<!-- RTP ICONT START -->
+<div style="position: fixed; bottom: 70px; left: 5px; z-index: 10; opacity: 0.98;">
+    <a href="http://LUCKYPOCKY88indo.com/RTP" target="_blank" rel="noopener"><img class="wabutton" src="https://i.imgur.com/70KE3It.gif" alt="RTP LUCKYPOCKY88" width="80" height="80">
+    </a>
+    </div>
+    
+   <!-- <div style="position: fixed; bottom: 165px; left: 5px; z-index: 10; opacity: 0.98;">
+    <a href="https://surkale.me/Rtp" target="_blank" rel="noopener">
+    <img class="wabutton" src="https://imgku.io/download/VM0XeCIE.gif" alt="RTP OLXTOTO" width="80" height="80">
+    </a>
+    </div> -->
+<!-- RTP ICONT END -->
+<!-- START: NAVBAR DESKTOP -->
+<div class="d-none d-sm-none d-md-block d-md-none">
+    <nav class="contain navbar fixed-top" style="background-color:#000000;margin:0 auto;">
+        <div class="col col-md-4 text-left">
+            <a class="navbar-brand">
+                <amp-img alt="LUCKYPOCKY88" src="assets/images/logo.png" width="230px" height="60px" layout="fixed">
+            </amp-img></a>
+        </div>
+        <div class="col col-md-8 col-sm-12 text-right">
+            <a class="btn btn-theme" href="https://watitoto124.com/index.html#/mine" style="width:150px;">LOGIN</a>
+            &nbsp;<a class="btn btn-custom btn-theme" href="http://LUCKYPOCKY88indo.com/WT2" style="width:150px;">DAFTAR</a>
+        </div>
+    </nav>
+</div>
+<!-- END -->
+<!-- START: NAVBAR MOBILE -->
+<nav class="navbar fixed-top navbar-brand pb-1 pt-1 text-center d-md-none d-lg-none d-xl-none" aria-label="home" style="background-color: #000000;width:100%;height:95px;padding-top:22px;padding-left:50px;">
+    <amp-img alt="LUCKYPOCKY88" src="assets/images/logo.png" width="160" height="55"></amp-img>
+</nav>
+<!-- END -->
+<!-- START: EMPTY -->
+<div class="spacer"></div>
+<br>
+<!-- END -->
+<div class="contain">
+    <div class="marquee-text">
+        <div style="width:100%;height:30px;background-color:#000000;padding:1px 10px 1px 10px;">
+            <p style="color:#fff;justify-content: center;">
+                <i class="fa fa-volume-up" aria-hidden="true"></i>
+                &nbsp;Selamat Datang Di LUCKYPOCKY88 - Agen Togel Online Terbaik Dan Terpercaya Masa Kini - Selamat Bergabung Dan Semoga Beruntung &nbsp;&nbsp;&nbsp;RAIH KEMENANGAN DAN NIKMATI BONUS PROMO HANYA DI SITUS LUCKYPOCKY88 &nbsp;&nbsp;&nbsp;
+            </p>
+        </div>
+    </div>
+    <!-- START: SLIDESHOW -->
+    <section>
+        <amp-carousel loop="" autoplay="" delay="4000" width="1199" height="514" layout="responsive" type="slides">
+            <amp-img src="assets/images/slideshow/PHONE4.jpg" alt="togel resmi" layout="responsive" width="300" height="150"></amp-img>
+            <amp-img src="assets/images/slideshow/PHONE1.jpg" alt="togel resmi" layout="responsive" width="300" height="150"></amp-img>
+            <amp-img src="assets/images/slideshow/PHONE2.jpg" alt="togel resmi" layout="responsive" width="300" height="150"></amp-img>
+            <amp-img src="assets/images/slideshow/PHONE3.jpg" alt="togel resmi" layout="responsive" width="300" height="150"></amp-img>
+        </amp-carousel>
+    </section>
+    <!-- END -->
+</div>
+<div class="contain bg-white ptpb">
+    <div class="text-center">
+        <p>
+            <b style="font-size: 50px;">LIVE CHAT PRIORITAS 24 JAM</b>
+            <br>LUCKYPOCKY88 selalu memberikan pelayanan terbaik dan menyenangkan melayani 7 x 24 Jam. Hubungi kami jika anda memiliki pertanyaan apapun kendala anda.
+      
+        </p>
+        <a href="https://wa.me/+6281266833683" class="btn btn-theme" rel="”nofollow”" target="_blank" style="width:150px;">
+            <i class="fa fa-comments" aria-hidden="true"></i>
+            &nbsp;&nbsp;CHAT PRIORITAS
+      
+        </a>
+    </div>
+</div>
+<div class="contain bg-white ptpb">
+    <amp-img alt="LUCKYPOCKY88" src="assets/images/DOWN1.jpg" srcset="assets/images/DOWN1.jpg" width="655" height="100" layout="responsive"></amp-img>
+    <br>
+    <div class="contain pt-2 full-width ptpb bg-white" style="border:2px solid #ccc;">
+        <div class="row">
+            <div class="col col-md-4 col-sm-4">
+                <p style="font-size: 14px;">
+                    Link Alternatif 1<br>🇰🇭 Server Kamboja
+          
+                </p>
+            </div>
+            <div class="col col-md-4 col-sm-4 text-center text-center">
+                <div class="text-center text-success pt-3">
+                    <span class="spinner-grow spinner-grow-sm"></span>
+                    &nbsp;<b>ONLINE</b>
+                </div>
+            </div>
+            <div class="col col-md-4 col-sm-12 text-right">
+                <a class="btn btn-theme my-2 my-sm-0" href="http://LUCKYPOCKY88indo.com/WT2">
+                    <i class="fa fa-globe" aria-hidden="true"></i>
+                    &nbsp;LOGIN
+          
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-4">
+                <p style="font-size: 14px;">
+                    Link Alternatif 2<br>🇯🇵 Server Jepang
+          
+                </p>
+            </div>
+            <div class="col col-md-4">
+                <div class="text-center text-success pt-3">
+                    <span class="spinner-grow spinner-grow-sm"></span>
+                    &nbsp;<b>ONLINE</b>
+                </div>
+            </div>
+            <div class="col col-md-4 text-right">
+                <a class="btn btn-theme my-2 my-sm-0" href="http://LUCKYPOCKY88indo.com/WT2">
+                    <i class="fa fa-globe" aria-hidden="true"></i>
+                    &nbsp;LOGIN
+          
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-4">
+                <p style="font-size: 14px;">
+                    Link Alternatif3<br>🇹🇭 Server Thailand
+          
+                </p>
+            </div>
+            <div class="col col-md-4 text-center">
+                <div class="text-center text-success pt-3">
+                    <span class="spinner-grow spinner-grow-sm"></span>
+                    &nbsp;<b>ONLINE</b>
+                </div>
+            </div>
+            <div class="col col-md-4 text-right">
+                <a class="btn btn-theme my-2 my-sm-0" href="http://LUCKYPOCKY88indo.com/WT2">
+                    <i class="fa fa-globe" aria-hidden="true"></i>
+                    &nbsp;LOGIN
+          
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-4">
+                <p style="font-size: 14px;">
+                    Link Alternatif3<br>🇺🇸 Server Amerika
+          
+                </p>
+            </div>
+            <div class="col col-md-4 text-center">
+                <div class="text-center text-success pt-3">
+                    <span class="spinner-grow spinner-grow-sm"></span>
+                    &nbsp;<b>ONLINE</b>
+                </div>
+            </div>
+            <div class="col col-md-4 text-right">
+                <a class="btn btn-theme my-2 my-sm-0" href="http://LUCKYPOCKY88indo.com/WT2">
+                    <i class="fa fa-globe" aria-hidden="true"></i>
+                    &nbsp;LOGIN
+          
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-4">
+                <p style="font-size: 13px;">
+                    Link Alternatif4<br>🇮🇩 Server Indonesia
+          
+                </p>
+            </div>
+            <div class="col col-md-4 text-center">
+                <div class="text-center text-danger pt-3">
+                    ⓧ&nbsp;<b>OFFLINE</b>
+                </div>
+            </div>
+            <div class="col col-md-4 text-right">
+                <button disabled="" class="btn btn-danger my-2 my-sm-0">
+                    <i class="fa fa-globe" aria-hidden="true"></i>
+                    &nbsp;LOGIN
+          
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--<div class="contain ptpb" style="background-image: url('./assets/images/bg-lef.webp'); background-repeat: no-repeat;background-size:cover;padding-top:20px;padding-bottom:20px;">-->
+<!--  <div class="text-center">-->
+<!--    <p style="color:#fff;">-->
+<!--      <b style="font-size: 45px;">Aplikasi LUCKYPOCKY88</b><br />-->
+<!--      LUCKYPOCKY88 menghadirkan kenyamanan dan efisiensi Terbaik dalam bermain Togel dan Slot hanya dalam satu genggaman Aplikasi saja.-->
+<!--    </p>-->
+<!--<a href="https://rebrand.ly/TOGELUP-APK" class="btn btn-theme" rel=”nofollow” style="width:150px;">-->
+<!--  <i class="fa fa-android" aria-hidden="true"></i>&nbsp;&nbsp;DOWNLOAD .APK-->
+<!--</a>-->
+<!--  </div>-->
+<!--</div>-->
+<div class="contain">
+    <div class="card text-center ptpb">
+        <div class="pt-2 pl-2 pr-2" style="background-color: #ccc;">
+            <h1 style="font-size:22px;">Akun Togel Terpercaya | Link Alternatif LUCKYPOCKY88 | Bebas Nawala</h1>
+        </div>
+        <div class="text-left pt-4 pl-4 pr-4">
+            <p>
+                 <a href="http://LUCKYPOCKY88indo.com/WT2">LUCKYPOCKY88 adalah situs resmi bola</a>
+                 togel, slot livegames terlengkap dan terpercaya di tanah air.
+                 LUCKYPOCKY88 di beri gelar salah satu BO terpercaya dengan tingkat pelayanan dan keamanan terbaik.
+                 dengan pelayanan online 24 jam, proses deposit serta withdraw yang sangat cepat diproses, dan keamanan tingkat tinggi dalam data pemain hingga menyebabkan pemain nyaman dan tenang bermain di situs LUCKYPOCKY88.
+<br>
+                <br>
+                Tidak hanya itu, LUCKYPOCKY88 juga menyediakan pemain dalam mengakses beberapa link alternaltif terbaik anti nawala, anti lelet.
+                Beberapa hal juga yang akan membuat pemain di LUCKYPOCKY88 menjadi sangat favorit adalah dengan adanya group prediksi di telegram secara gratis, Rtp slot anti rungkat beserta polanya hingga membuat tingkat kemenangan pemain semakin besar.
+<br>
+                <br>
+                Dengan adanya tingkat winrate kemenangan tinggi itu bisa membuat setiap pemain yang bergabung didalam nya mendapatkan kemenangan yang mutlak. 
+yang lebih pastinya adalah , JP berapapun tetap dibayar.
+<br>
+                <br>Info togel hongkong, bandar togel terbaik, cek togel singapore, judi togel terpercaya, daftar togel singapore, togel wap original, togel singapore online, togel seratus rupiah, keluaran lengkap togel, daftar togel terpercaya, togel singapore terpercaya, agen togel terbesar, bandar togel terbesar, togel singapore hongkong, aplikasi togel terpercaya.
+    
+            </p>
+        </div>
+    </div>
+</div>
+<div class="contain pb-4 pt-4" style="background-color:#000000;">
+    <p style="color:#fff;text-align:center;font-size:40px;">Temukan Kami Disini</p>
+    <div class="row pt-4 pb-4" style="width:80%;margin:0 auto;background-color:#000000;">
+        <div class="col col-md-3 col-sm-3 col-xs-3 text-center">
+            <a rel="”nofollow”" target="_blank" href="https://www.facebook.com/LUCKYPOCKY88ttm/">
+                <span class="text-white" style="font-size: 11px;">
+                    <i class="fa fa-facebook-square" aria-hidden="true" style="font-size:30px;"></i>
+                    <br>FACEBOOK
+                </span>
+            </a>
+        </div>
+        <div class="col col-md-3 col-sm-3 col-xs-3 text-center">
+            <a rel="”nofollow”" target="_blank" href="https://t.me/watitotoLUCKYPOCKY88">
+                <span class="text-white" style="font-size: 11px;">
+                    <i class="fa fa-telegram" style="font-size:30px;color:rgb(255, 255, 255)"></i>
+                    <br>TELEGRAM
+                </span>
+            </a>
+        </div>
+        <div class="col col-md-3 col-sm-3 col-xs-3 text-center">
+            <a rel="”nofollow”" target="_blank" href="http://LUCKYPOCKY88indo.com/WT2">
+                <span class="text-white" style="font-size: 11px;">
+                    <i class="fa fa-instagram text-white" aria-hidden="true" style="font-size:30px;"></i>
+                    <br>INSTAGRAM
+                </span>
+            </a>
+        </div>
+        <div class="col col-md-3 col-sm-3 col-xs-3 text-center">
+            <a rel="”nofollow”" target="_blank" href="https://www.youtube.com/@livedrawLUCKYPOCKY8814/videos">
+                <span class="text-white" style="font-size: 11px;">
+                    <i class="fa fa-youtube-play text-white" aria-hidden="true" style="font-size:30px;"></i>
+                    <br>YOUTUBE
+                </span>
+            </a>
+        </div>
+    </div>
+    <div class="text-center">
+        <p style="color:#fff;font-size:14px;width:90%;margin:0 auto;">LUCKYPOCKY88 selalu memberikan pelayanan terbaik dan menyenangkan melayani 7 x 24 Jam. Hubungi kami jika anda memiliki pertanyaan apapun kendala anda.
+      </p>
+        <!-- <a class="btn btn-warning"  style="width:150px;">
+        <i class="fa fa-comments" aria-hidden="true"></i>&nbsp;&nbsp;LIVE CHAT
+      </a> -->
+    </div>
+    <div class="pt-4 pb-4">
+        <p style="color:#fff;text-align:center;font-size:14px;">Copyright 2024 - LUCKYPOCKY88, All Rights Reserved.</p>
+    </div>
+</div>
+</body>
+</html>
